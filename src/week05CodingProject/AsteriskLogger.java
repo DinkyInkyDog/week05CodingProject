27271624275e370cd6d37a0c6d2335da2e4f6c18
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-05-04 08:37:04 -0400

    add comments

--- src/week05CodingProject/AsteriskLogger.java
+++ src/week05CodingProject/AsteriskLogger.java
@@ -2,13 +2,14 @@ package week05CodingProject;
 
 public class AsteriskLogger implements Logger {
 
-
+//in order for the class to connect to the interface you need to have implements interfaceName after the class name.
 
 	@Override
 	public void log(String log) {
 		System.out.println("***" + log + "***");
 
 	}
+	//the @override is just letting us know that the method came from the interface.
 
 	@Override
 	public void error(String error) {
